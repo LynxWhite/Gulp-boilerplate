90eaa8df6180bd32ac3e03856f592abb5af3c68d
Anton Varlamov <vad@barl.ru>
2018-04-02 13:00:34 +0300

    useless tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 'use strict'
 
-const gulp = require('gulp')
+const gulp = require('gulp'),
+      sass = require('gulp-sass')
 
-gulp.task('default', function() {
-    console.log('task')
+gulp.task('sass', function() {
+    return gulp.src('app/static/sass/**/*.sass')
+    .pipe(sass())
+    .pipe(gulp.dest('app/static/css'))
+})
+
+gulp.task('html', function() {
+    return gulp.src('app/*.html')
+    .pipe(gulp.dest('dist'))
 })
