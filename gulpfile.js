5de91e3dbe993da002e128260b10a4c8a1e6265e
Anton Varlamov <vad@barl.ru>
2018-04-03 10:27:20 +0300

    added autoprefixer, notify, soucemaps

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,19 @@
 const gulp = require('gulp'),
       sass = require('gulp-sass'),
       stylus = require('gulp-stylus'),
-      csso  = require('gulp-csso')
+      csso  = require('gulp-csso'),
+      autoprefixer = require('gulp-autoprefixer'),
+      notify = require('gulp-notify'),
+      sourcemaps = require('gulp-sourcemaps')
+
+// csso - минификация css
+// autoprefixer - дописывает префиксы для разных версий браузеров
+// notify - обработка ошибок
 
 gulp.task('sass', function() {
     return gulp.src('app/static/sass/**/*.sass')
     .pipe(sass())
+    .pipe(csso())
     .pipe(gulp.dest('app/static/css'))
 })
 
@@ -18,7 +26,17 @@ gulp.task('html', function() {
 
 gulp.task('stylus', function() {
     return gulp.src('app/static/stylus/main.styl')
+    .pipe(sourcemaps.init())
     .pipe(stylus())
+    .pipe(autoprefixer({
+        browsers: ['last 10 versions']
+    }))
+    .on('error', notify.onError({
+        message: 'Error: <%= error.message %>',
+        tittle: "Error running stylus task"
+    }))
     .pipe(csso())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest('dist/static/css'))
 })
+
